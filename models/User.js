0e86cbe9b820c05260711c831fd77cebb674e0ad
Catherine <catheneghan@gmail.com>
2021-09-13 11:16:55 -0700

    used sequalize to create comment and vote tables. Finished database and api routes

--- models/User.js
+++ models/User.js
@@ -6,7 +6,7 @@ const bcrypt = require('bcrypt');
 class User extends Model { 
     checkPassword(loginPw) {
         return bcrypt.compareSync(loginPw, this.password);
-      }
+    }
 }
 
 // define table columns and configuration
