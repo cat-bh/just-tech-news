c71ed0802f8ea6a72ee0edf3b6b238d81e67053b
Catherine <catheneghan@gmail.com>
2021-09-14 13:44:05 -0700

    created template to show posts on homepage

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
-const express = require('express');
-const routes = require('./routes');
-const sequelize = require('./config/connection');
 const path = require('path');
+const express = require('express');
+const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const sequelize = require('./config/connection');
+
+const hbs = exphbs.create({});
+
+const routes = require('./controllers/');
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -15,6 +23,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 // turn on connection to db and server
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
